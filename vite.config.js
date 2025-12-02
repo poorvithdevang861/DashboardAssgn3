9d273aa0c62b22ba91294caf16c041c4c3c37667
Poorvith Devang <poorvithdevang@Poorviths-MacBook-Air.local>
2025-12-02 18:50:18 +0530

    init

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/crime-dashboard/',
+  base: '/DashboardAssgn3/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
